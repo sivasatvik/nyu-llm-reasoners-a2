eac18ff1fa66776284406b5a0fc58ddda8b26647
Siva Satvik Mandapati <sivasatvik@gmail.com>
2026-03-02 15:44:22 -0500

    Minor fix in scripts

--- student/benchmarking_scripts/run_memory_profiles_large_mixed.sh
+++ student/benchmarking_scripts/run_memory_profiles_large_mixed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Run BF16 mixed-precision memory profiling for xl model.
+# Run BF16 mixed-precision memory profiling for large model.
 
 set -euo pipefail
 
-MIXED=1 bash student/benchmarking_scripts/run_memory_profiles_xl.sh
+MIXED=1 bash student/benchmarking_scripts/run_memory_profiles_large.sh
